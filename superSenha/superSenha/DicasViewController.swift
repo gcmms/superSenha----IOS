69622600d385f8c2699dad0706df883e78578113
MacBookFcamara <32177705+gcmms@users.noreply.github.com>
2019-07-02 13:02:53 -0300

    continuação

---
+++ superSenha/superSenha/DicasViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class DicasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func Fechar(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+
+}
